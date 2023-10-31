e61c0e5fedd3e95ccd2e4c61cda731e03a92cf5d
s2201115 <s2201115@sendai-nct.jp>
2023-10-31 22:41:46 +0900

    参考ファイル

---
+++ write.js
@@ -0,0 +1,10 @@
+function write_Alpha() {
+    target=document.getElementById("output");
+    target.innerHTML="A";
+   }
+
+function write_Kana() {
+    target=document.getElementById("output");
+    target.innerHTML="あ";
+   }
+   
